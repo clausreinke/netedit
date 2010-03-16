f4789cfa93118f0e19f098cd1104247ab3c7394e
claus.reinke <claus.reinke@talk21.com>
2010-03-16 16:36:09 +0000

    initial check-in of a simple petri-net editor

---
+++ js/debug.js
@@ -0,0 +1,42 @@
+function listAttributes(pre,x) {
+  message('// ------------- listing attributes for '+pre);
+  for (var a in x.attributes) {
+    message(pre+a+" = "+x.getAttribute(a));
+    if (a && a.value)
+      message("// "+pre+a+".value : "+a.value);
+  }
+}
+
+function listProperties(pre,x) {
+  message('// ------------- listing properties for '+pre);
+  for (var key in x) {
+    p = x[key];
+    if (p && (typeof p != "function"))
+      message(pre+key+"("+typeof p+") : "+p);
+    if (p && p.value)
+      message("// "+pre+key+".value : "+p.value);
+  }
+}
+
+function message(msg) {
+  msgs = document.getElementById('messages');
+  msgs.appendChild(document.createTextNode(msg));
+  msgs.appendChild(document.createElement("br"));
+}
+
+function listSVG(id) {
+  var svg     = document.getElementById(id);
+  var console = document.getElementById('console');
+  console.appendChild(document.createTextNode('listing svg "'+id+'"'));
+  console.appendChild(document.createElement("br"));
+  for (var c in svg.children) {
+    child = svg.children[c];
+    if (typeof child != "function") {
+      cls = child.className ? child.className.baseVal : 'none';
+      console.appendChild(document.createTextNode(c+" "+child+" - "+cls));
+      console.appendChild(document.createElement("br"));
+    }
+  }
+}
+
+
