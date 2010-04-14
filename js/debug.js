dc6112ac666651349aa861c2b4f36b5b048f91aa
claus.reinke <claus.reinke@talk21.com>
2010-04-14 22:58:10 +0000

    refinements

--- js/debug.js
+++ js/debug.js
@@ -1,27 +1,27 @@
 
 // use document element 'messages' as an output console
-function message(msg) {
-  msgs = document.getElementById('messages');
+function message(msg,div) {
+  msgs = document.getElementById(div==null?'messages':div);
   msgs.appendChild(document.createTextNode(msg));
   msgs.appendChild(document.createElement("br"));
 }
 
 // wrap object methods, for debugging purposes, with optional before code;
-// examples:
-//
-//  // just report calls to obj.method, with arguments
-//  wrap(obj.prototype,'method'); 
-//  // report calls to obj.method, with arguments, 
-//  // and output more specific info about arguments
-//  wrap(obj.prototype,'method',function(arg1,arg2) { message(..); });
-function wrap(x,f,before) {
+function wrap(x,f,advice) {
   var wrapper = function(oldf) {
     return function() { 
-      var args = '';
-      for(var i=0; i<arguments.length; i++) args += ','+arguments[i];
-      message('calling '+f+'('+args.substring(1)+')'); 
-      if (before) before.apply(this,arguments);
-      return oldf.apply(this,arguments); };
+      var args = [];
+      for(var i=0; i<arguments.length; i++) args.push(arguments[i]);
+      // message('calling '+f+'('+args.join(',')+')'); 
+      if (advice.before) advice.before.apply(this,arguments);
+      if (advice.around) {
+        args.unshift(oldf);
+        var result = advice.around.apply(this,args);
+      } else
+        var result = oldf.apply(this,arguments); 
+      if (advice.after) advice.after.apply(this,arguments);
+      return result;
+      };
     };
 
   if ((f in x) && ((typeof x[f])==='function')) {
@@ -33,11 +33,12 @@ function wrap(x,f,before) {
 }
 
 // TODO: marginally useful so far, needs elaboration
-function listProperties(pre,x) {
+function listProperties(pre,x,without) {
   message('// ----start---- listing properties for '+pre);
   for (var key in x) {
     p = x[key];
-    if (p!==null && p!=='' && (typeof p != "function")) {
+    if (p!==null && p!=='' 
+        && (without!==null || (without.indexOf(typeof p)===-1))) {
       message(pre+'.'+key+"("+typeof p+") : "+p);
       if (p.name)
         message("// "+pre+'.'+key+".name : "+p.name);
@@ -46,6 +47,58 @@ function listProperties(pre,x) {
     }
   }
   message('// ----end------ listing properties for '+pre);
+}
+
+function ObjectViewer(consoleID) {
+  var console = document.getElementById(consoleID);
+
+  var form = document.createElement('form');
+  var show = document.createElement('input');
+  show.type = 'submit'; show.value = 'show';
+  var clear = document.createElement('input');
+  clear.type = 'submit'; clear.value = 'clear';
+  var commandline = document.createElement('input');
+  commandline.type = 'text'; commandline.maxlength = '100';
+  commandline.style.width = '80%';
+  document.body.insertBefore(form,document.body.firstChild);
+
+  form.appendChild(commandline);
+  form.appendChild(show);
+  form.appendChild(clear);
+  form.action = '#';
+  show.addEventListener('click',function(event) {
+      message(commandline.value,consoleID);
+      var node = eval(commandline.value);
+      for (var p in node) {
+        message(commandline.value+'.'+p+': '+node[p],consoleID);
+      }
+    },false);
+  clear.addEventListener('click',function(event) {
+        while (console.hasChildNodes())
+          console.removeChild(console.firstChild);
+    },false);
+}
+
+function JSEval(consoleID) {
+  var console = document.getElementById(consoleID);
+
+  var form = document.createElement('form');
+  var button = document.createElement('input');
+  button.type = 'submit'; button.value = 'eval';
+  var commandline = document.createElement('input');
+  commandline.type = 'text'; commandline.maxlength = '100';
+  commandline.style.width = '80%';
+  document.body.insertBefore(form,document.body.firstChild);
+
+  form.appendChild(commandline);
+  form.appendChild(button);
+  form.action = '#';
+  button.addEventListener('click',function(event) {
+      message(commandline.value,consoleID);
+      try { var result = ' = '+eval(commandline.value); } 
+      catch (e) { var result = ' ! '+e; };
+      message(result,consoleID);
+    },false);
 }
 
 // ------------ miscellaneous experiments
@@ -60,21 +113,6 @@ function listAttributes(pre,x,ns) {
                      +' | '+xa.name+'='+xa.value);
     if (a && a.value)
       message("// "+pre+a+".value : "+a.value);
-  }
-}
-
-function listSVG(id) {
-  var svg     = document.getElementById(id);
-  var console = document.getElementById('console');
-  console.appendChild(document.createTextNode('listing svg "'+id+'"'));
-  console.appendChild(document.createElement("br"));
-  for (var c in svg.children) {
-    child = svg.children[c];
-    if (typeof child != "function") {
-      cls = child.className ? child.className.baseVal : 'none';
-      console.appendChild(document.createTextNode(c+" "+child+" - "+cls));
-      console.appendChild(document.createElement("br"));
-    }
   }
 }
 
